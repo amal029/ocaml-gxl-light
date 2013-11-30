d7846e5b7d15a7374081ede0eea80f21d07c2de8
Avinash Malik <avinash.malik@auckland.ac.nz>
2013-12-01 11:01:53 +1300

    Updated the META file

--- test.ml
+++ test.ml
@@ -4,20 +4,20 @@
 
 open GxlDocument;;
 
-let pp = GxlDocument.make();;
+let parser = GxlDocument.make();;
 
 (* Parse an attributed file *)
-(* let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/attredges/attredges.gxl" parser;; *)
-(* let () = GxlDocument.write "/tmp/g_atteredges.gxl" parser;; *)
+let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/attredges/attredges.gxl" parser;;
+let () = GxlDocument.write "/tmp/g_atteredges.gxl" parser;;
 
-(* (\* Parse a complex attributed file *\) *)
-(* let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/complexExample/complexExample.gxl" parser;; *)
-(* let () = GxlDocument.write "/tmp/g_complexExample.gxl" parser;; *)
+(* Parse a complex attributed file *)
+let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/complexExample/complexExample.gxl" parser;;
+let () = GxlDocument.write "/tmp/g_complexExample.gxl" parser;;
 
-(* (\* Parse a hyper-graph file*\) *)
-(* let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/hypergraph/hypergraph.gxl" parser;; *)
-(* let () = GxlDocument.write "/tmp/g_hypergraph.gxl" parser;; *)
+(* Parse a hyper-graph file*)
+let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/hypergraph/hypergraph.gxl" parser;;
+let () = GxlDocument.write "/tmp/g_hypergraph.gxl" parser;;
 
-(* (\* Parse a hierarchical-graph file*\) *)
-let () = GxlDocument.parse "/home/amal029/AUCKLAND_WORK/Oliver/Fork_Join_Nodes_30_CCR_1.99_WeightType_Random.gxl" pp
-let () = GxlDocument.write "/tmp/tutu.gxl" pp
+(* Parse a hierarchical-graph file*)
+let () = GxlDocument.parse "/tmp/gxl-1.0-examples/instance/gxl/hierarchicalGraph/hierarchicalGraph.gxl" parser;;
+let () = GxlDocument.write "/tmp/g_hierarchicalGraph.gxl" parser;;
